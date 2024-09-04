3eb51f206f33435ef2ac038eea2c5820a177c6b2
SAValenciaA <savalenciaan44@gmail.com>
2024-09-04 11:58:04 -0500

    formatting

--- translations.py
+++ translations.py
@@ -1,5 +1,5 @@
 import os
-from random import shuffle, sample
+from random import sample, shuffle
 
 # This code is just for copying the file into a dictionary
 
@@ -16,23 +16,21 @@ RED = "\033[0;31m"
 YELLOW = "\033[33m"
 WHITE = "\033[0m"
 
-fileLines = open(f'{PWD}/data/words').read().split("\n")
+fileLines = open(f"{PWD}/data/words").read().split("\n")
 
 translations = [i.split(" | ") for i in fileLines]
-translationsLevel = lambda lvl: translations[lvl*50:lvl*50 + 50]
+translationsLevel = lambda lvl: translations[lvl * 50 : lvl * 50 + 50]
 
 translationsList = lambda lvl: [
-    {
-        "word": line[0].lower(),
-        "answers": set(line[1].lower().split(","))
-    } 
+    {"word": line[0].lower(), "answers": set(line[1].lower().split(","))}
     for line in translationsLevel(lvl)
 ]
 
+
 def printScore(lvl, score, width):
     os.system("clear")
-    print(WHITE + "_"*width)
-    file = open(f'{PWD}/data/{score}')
+    print(WHITE + "_" * width)
+    file = open(f"{PWD}/data/{score}")
     lines = file.read().split("\n")
     top = 0
     maxTop = 10
@@ -42,7 +40,8 @@ def printScore(lvl, score, width):
             top += 1
         if top == maxTop:
             break
-    print(WHITE + "_"*width)
+    print(WHITE + "_" * width)
+
 
 def evaluateUser(translation, mode, width):
 
@@ -50,21 +49,25 @@ def evaluateUser(translation, mode, width):
     word = translation["word"]
 
     # Separator
-    print(WHITE + ("_"*width))
+    print(WHITE + ("_" * width))
 
     if mode == "TRANSLATE TO ENGLISH":
 
-        print(f'Your clue is: {word}')
+        print(f"Your clue is: {word}")
         isCorrectResponse = lambda response: response in answers
         validResponses = ", ".join(answers)
-        printCorrection = lambda: print(f'It could have been: {validResponses}')
-        
+        printCorrection = lambda: print(f"It could have been: {validResponses}")
+
     elif mode == "TRANSLATE TO FRENCH":
 
-        clue = list(answers)[0] if len(answers) == 1 else ", ".join(sample(list(answers),2))
-        print(f'Your clue is: {clue}')
+        clue = (
+            list(answers)[0]
+            if len(answers) == 1
+            else ", ".join(sample(list(answers), 2))
+        )
+        print(f"Your clue is: {clue}")
         isCorrectResponse = lambda response: response == word
-        printCorrection = lambda: print(f'It should have been: {word}')
+        printCorrection = lambda: print(f"It should have been: {word}")
 
     response = input("Answer: ")
 
@@ -76,4 +79,3 @@ def evaluateUser(translation, mode, width):
         print(RED + "False!")
         printCorrection()
         return 0
-
